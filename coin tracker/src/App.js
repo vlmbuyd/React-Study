bfdd9392c79249fe6443057df28bceb64fb395a4
Yean <kjan0929@naver.com>
2024-04-18 23:40:37 +0900

    bitcoin you can buy in dollars [error]

--- coin tracker/src/App.js
+++ coin tracker/src/App.js
@@ -3,6 +3,13 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [dollars, setDollars] = useState("");
+  const onChange = (event) => setDollars(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(dollars);
+    // setDollars("");
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -14,6 +21,14 @@ function App() {
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={dollars}
+          onChange={onChange}
+          placeholder="Write the dollar"
+        ></input>
+      </form>
+      <br></br>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
@@ -25,6 +40,18 @@ function App() {
           ))}
         </select>
       )}
+
+      <ul>
+        {coins.map((coin, index) => {
+          dollars > coin ? (
+            <li key={index}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </li>
+          ) : (
+            <li>You can't buy any coin</li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
