0df860e1f21c6227ee98ebc495bec5340784846d
Yean <kjan0929@naver.com>
2024-04-19 21:53:33 +0900

    Get API

---
+++ movie app/src/App.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+}
+
+export default App;
