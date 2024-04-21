d643826cabc4a73c56e23031a96aa2b7150ee42a
Yean <kjan0929@naver.com>
2024-04-21 10:29:43 +0900

    button dynamic styling

--- styled component/src/App.js
+++ styled component/src/App.js
@@ -1,26 +1,30 @@
 import styled from "styled-components";
+import Button from "./Button";
 import Input from "./Input";
 
 const Container = styled.div`
-  margin: 0 auto;
-  width: 400px;
-
-  ${Input} {
-    box-sizing: border-box;
-    display: block;
-    margin: 8px 0 16px;
-    width: 100%;
+  ${Button} {
+    margin: 10px;
   }
 `;
 
 function App() {
   return (
     <Container>
-      <h1>로그인</h1>
-      <label htmlFor="email">Email</label>
-      <Input id="email" placeholder="styled@codeit.kr"></Input>
-      <label htmlFor="password">비밀번호</label>
-      <Input id="password" placeholder="비밀번호"></Input>
+      <h1>기본 버튼</h1>
+      <Button size="small">small</Button>
+      <Button size="medium">medium</Button>
+      <Button size="large">large</Button>
+      <h1>둥근 버튼</h1>
+      <Button size="small" round>
+        round small
+      </Button>
+      <Button size="medium" round>
+        round medium
+      </Button>
+      <Button size="large" round>
+        round large
+      </Button>
     </Container>
   );
 }
