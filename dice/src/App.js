9400e6fbef0721db6ca2c1053116ecfa262491b4
Yean <kjan0929@naver.com>
2024-05-04 18:57:52 +0900

    random dice roll

--- dice/src/App.js
+++ dice/src/App.js
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import Button from "./Button";
 import Dice from "./Dice";
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
 function App() {
+  const [num, setNum] = useState(1);
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+  };
+
   return (
     <div>
       <div>
-        <Button>던지기</Button>
+        <Button onClick={handleRollClick}>던지기</Button>
         <Button>처음부터</Button>
       </div>
-      <Dice color="red" num={4} />
+      <Dice color="red" num={num} />
     </div>
   );
 }
