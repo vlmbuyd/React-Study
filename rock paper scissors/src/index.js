f9e3773889b11807b4ba939bbd64ac881926ba20
Yean <kjan0929@naver.com>
2024-05-04 16:11:16 +0900

    first commit

--- rock paper scissors/src/index.js
+++ rock paper scissors/src/index.js
@@ -1,12 +1,32 @@
 import { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 
+const WINS = {
+  rock: "scissor",
+  scissor: "paper",
+  paper: "rock",
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return "승리";
+  else if (left === WINS[right]) return "패배";
+  return "무승부";
+}
+
+function handleClick() {
+  console.log("가위바위보!");
+}
+
+const me = "rock";
+const other = "scissor";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <h1 id="title">가위바위보</h1>
-    <button className="hand">가위</button>
-    <button className="hand">바위</button>
-    <button className="hand">보</button>
+    <h1>가위바위보</h1>
+    <h2>{getResult(me, other)}</h2>
+    <button onClick={handleClick}>멍멍</button>
+    <button onClick={handleClick}>바보</button>
+    <button onClick={handleClick}>똥 </button>
   </>
 );
