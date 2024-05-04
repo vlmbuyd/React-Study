ac6d99609249efe455f92f07b2a6d898167aed8e
Yean <kjan0929@naver.com>
2024-05-04 17:04:51 +0900

    display images on screen

--- rock paper scissors/src/index.js
+++ rock paper scissors/src/index.js
@@ -1,32 +1,6 @@
 import { Fragment } from "react";
 import ReactDOM from "react-dom/client";
-
-const WINS = {
-  rock: "scissor",
-  scissor: "paper",
-  paper: "rock",
-};
-
-function getResult(left, right) {
-  if (WINS[left] === right) return "승리";
-  else if (left === WINS[right]) return "패배";
-  return "무승부";
-}
-
-function handleClick() {
-  console.log("가위바위보!");
-}
-
-const me = "rock";
-const other = "scissor";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <h1>가위바위보</h1>
-    <h2>{getResult(me, other)}</h2>
-    <button onClick={handleClick}>멍멍</button>
-    <button onClick={handleClick}>바보</button>
-    <button onClick={handleClick}>똥 </button>
-  </>
-);
+root.render(<App />);
