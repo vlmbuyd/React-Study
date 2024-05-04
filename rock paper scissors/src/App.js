e932a4124884865438c6917eedf98097b9d91b62
Yean <kjan0929@naver.com>
2024-05-04 18:13:42 +0900

    children

--- rock paper scissors/src/App.js
+++ rock paper scissors/src/App.js
@@ -1,9 +1,12 @@
+import Button from "./Button";
 import HandButton from "./HandButton";
 
 function App() {
   const handleClick = (value) => console.log(value);
+  const handleClearClick = () => console.log("처음부터");
   return (
     <div>
+      <Button onClick={handleClearClick}>처음부터</Button>
       <HandButton value="rock" onClick={handleClick} />
       <HandButton value="scissor" onClick={handleClick} />
       <HandButton value="paper" onClick={handleClick} />
