876a2a69304f0aa803923d65369f1b4cb03e33e5
Yean <kjan0929@naver.com>
2024-05-05 22:54:36 +0900

    Update the score according to the bet value received from the user

--- rock paper scissors/src/App.js
+++ rock paper scissors/src/App.js
@@ -17,27 +17,57 @@ function App() {
   const [hand, setHand] = useState(INITIAL_VALUE);
   const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
   const [gameHistory, setGameHistory] = useState([]);
+  const [score, setScore] = useState(0);
+  const [otherScore, setOtherScore] = useState(0);
+  const [bet, setBet] = useState(1);
 
   const handleButtonClick = (nextHand) => {
     const nextOtherHand = generateRandomHand();
-    const nextHistoryItem = getResult(hand, nextOtherHand);
+    const nextHistoryItem = getResult(nextHand, nextOtherHand);
+    const comparison = compareHand(nextHand, nextOtherHand);
     setHand(nextHand);
     setOtherHand(nextOtherHand);
     setGameHistory([...gameHistory, nextHistoryItem]);
+    if (comparison > 0) setScore(score + bet);
+    if (comparison < 0) setOtherScore(otherScore + bet);
   };
 
   const handleClearClick = () => {
     setHand(INITIAL_VALUE);
     setOtherHand(INITIAL_VALUE);
     setGameHistory([]);
+    setScore(0);
+    setOtherScore(0);
+    setBet(1);
   };
+
+  const handleBetChange = (e) => {
+    let num = Number(e.target.value);
+    if (num > 9) num %= 10;
+    if (num < 1) num = 1;
+    num = Math.floor(num);
+    setBet(num);
+  };
+
   return (
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
       <div>
+        {score} : {otherScore}
+      </div>
+      <div>
         <HandIcon value={hand}></HandIcon>
         VS
         <HandIcon value={otherHand}></HandIcon>
+      </div>
+      <div>
+        <input
+          onChange={handleBetChange}
+          type="number"
+          value={bet}
+          min={1}
+          max={9}
+        ></input>
       </div>
       <p>승부 기록 : {gameHistory.join(", ")}</p>
       <div>
