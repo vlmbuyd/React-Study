76daf302dbd6122eddeb1ac8b6a1f3e6d46a0b45
Yean <kjan0929@naver.com>
2024-05-05 20:53:46 +0900

    total score and log output using reference state

--- dice/src/App.js
+++ dice/src/App.js
@@ -9,16 +9,19 @@ function random(n) {
 function App() {
   const [num, setNum] = useState(1);
   const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
 
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
     setSum(sum + nextNum);
+    setGameHistory([...gameHistory, nextNum]);
   };
 
   const handleClarClick = () => {
     setNum(1);
     setSum(0);
+    setGameHistory([]);
   };
 
   return (
@@ -32,6 +35,7 @@ function App() {
         <Dice color="blue" num={num}></Dice>
         <h2>총점</h2>
         <p>{sum}</p>
+        <p>{gameHistory.join(", ")}</p>
       </div>
     </div>
   );
