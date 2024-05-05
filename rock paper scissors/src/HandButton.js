2fc64a567a4a2efed131adb0684dcbfdd506d751
Yean <kjan0929@naver.com>
2024-05-06 00:08:28 +0900

    apply inline style

--- rock paper scissors/src/HandButton.js
+++ rock paper scissors/src/HandButton.js
@@ -1,9 +1,24 @@
 import HandIcon from "./HandIcon";
+import purpleImg from "./assets/purple.svg";
+
+const style = {
+  width: "166px",
+  height: "166px",
+  border: "none",
+  outline: "none",
+  textAlign: "center",
+  cursor: "pointer",
+  backgroundColor: "transparent",
+  backgroundImage: `url("${purpleImg}")`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  backgroundSize: "contain",
+};
 
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value); // App 컴포넌트의 handleButtonClick 함수에게 value값 넘겨줌
   return (
-    <button onClick={handleClick}>
+    <button style={style} onClick={handleClick}>
       <HandIcon value={value}></HandIcon>
     </button>
   );
