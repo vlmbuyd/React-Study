850d798a68c0979292cba55afbb12caa29878987
Yean <kjan0929@naver.com>
2024-05-06 10:28:41 +0900

    applying style using classNames library

--- dice/src/Button.js
+++ dice/src/Button.js
@@ -1,9 +1,13 @@
+import classNames from "classnames";
 import "./Button.css";
 
 function Button({ children, onClick, color, className = "" }) {
-  const classNames = `Button ${color} ${className}`;
+  // const classNames = `Button ${color} ${className}`;
   return (
-    <button className={classNames} onClick={onClick}>
+    <button
+      className={classNames("Button", color, className)}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
