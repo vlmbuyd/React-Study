b37259800c7e032756303f3fc4d2b0972cda9f06
Yean <kjan0929@naver.com>
2024-05-06 09:45:05 +0900

    styling with css className

--- rock paper scissors/src/HandButton.js
+++ rock paper scissors/src/HandButton.js
@@ -1,25 +1,12 @@
 import HandIcon from "./HandIcon";
 import purpleImg from "./assets/purple.svg";
-
-const style = {
-  width: "166px",
-  height: "166px",
-  border: "none",
-  outline: "none",
-  textAlign: "center",
-  cursor: "pointer",
-  backgroundColor: "transparent",
-  backgroundImage: `url("${purpleImg}")`,
-  backgroundRepeat: "no-repeat",
-  backgroundPosition: "center",
-  backgroundSize: "contain",
-};
+import "./HandButton.css";
 
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value); // App 컴포넌트의 handleButtonClick 함수에게 value값 넘겨줌
   return (
-    <button style={style} onClick={handleClick}>
-      <HandIcon value={value}></HandIcon>
+    <button className="HandButton" onClick={handleClick}>
+      <HandIcon className="HandButton-icon" value={value}></HandIcon>
     </button>
   );
 }
--- rock paper scissors/src/HandIcon.js
+++ rock paper scissors/src/HandIcon.js
@@ -1,11 +1,12 @@
 import rockImg from "./assets/rock.svg";
 import scissorImg from "./assets/scissor.svg";
 import paperImg from "./assets/paper.svg";
+import "./HandButton.css";
 
-function HandIcon({ value }) {
+function HandIcon({ value, className }) {
   const src =
     value === "rock" ? rockImg : value === "scissor" ? scissorImg : paperImg;
-  return <img src={src} alt={value}></img>;
+  return <img className={className} src={src} alt={value}></img>;
 }
 
 export default HandIcon;
