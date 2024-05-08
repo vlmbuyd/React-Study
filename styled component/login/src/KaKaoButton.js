3f713ce82d447a9a016ee40287bc95e5ccefe2be
Yean <kjan0929@naver.com>
2024-05-08 16:58:23 +0900

    kakao button styling

--- styled component/login/src/KaKaoButton.js
+++ styled component/login/src/KaKaoButton.js
@@ -1,5 +1,35 @@
 import styled from "styled-components";
+import kakaoIcon from "./assets/kakao.svg";
+import Button from "./Button";
 
-const KaKaoButton = styled.button``;
+const Icon = styled.img`
+  width: 24px;
+  height: 24px;
+`;
+
+const StyledButton = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fee500;
+  color: rgba(0, 0, 0, 0.8);
+
+  ${Icon} {
+    margin-right: 8px;
+  }
+
+  &:hover {
+    background-color: #fee500;
+  }
+`;
+
+function KaKaoButton({ children }) {
+  return (
+    <StyledButton>
+      <Icon src={kakaoIcon} alt="kakao icon"></Icon>
+      {children}
+    </StyledButton>
+  );
+}
 
 export default KaKaoButton;
