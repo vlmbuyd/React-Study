6b8056395b6e2234602a81b85f73c55abe350610
Yean <kjan0929@naver.com>
2024-05-08 15:27:47 +0900

    header styling

--- styled component/login/src/App.js
+++ styled component/login/src/App.js
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+import codeitImg from "./assets/codeit.png";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 400px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+const Join = styled.span`
+  color: #848187;
+
+  & a {
+    color: #6500c3;
+    font-weight: bold;
+  }
+`;
+
+const Title = styled.div`
+  width: 200px;
+  height: 60px;
+  background: url(${codeitImg}) no-repeat;
+  background-position: center center;
+  background-size: 100% auto;
+`;
+
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <Container>
+      <Title></Title>
+      <Join>
+        회원이 아니신가요? <a href="#">회원가입 하기</a>
+      </Join>
+    </Container>
+  );
 }
 
 export default App;
