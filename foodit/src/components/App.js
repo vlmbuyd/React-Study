8fadc1c02b295afa195a4078d5cca50886b47b05
Yean <kjan0929@naver.com>
2024-05-10 23:48:17 +0900

    Sort latest, best rating

--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import FoodList from "../FoodList";
 import items from "../mock.json";
 
 function App() {
-  return <FoodList items={items}></FoodList>;
+  const [order, setOrder] = useState("createdAt");
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleCalorieClick = () => setOrder("calorie");
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  return (
+    <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleCalorieClick}>칼로리순</button>
+      </div>
+      <FoodList items={sortedItems}></FoodList>
+    </div>
+  );
 }
 
 export default App;
