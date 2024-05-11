00c0bd04a55ea6d8dd61c8a49020646f6ed91f14
Yean <kjan0929@naver.com>
2024-05-11 22:50:03 +0900

    Divide pages using Routes

--- codethat/src/Main.js
+++ codethat/src/Main.js
@@ -1,11 +1,25 @@
-import App from './components/App';
-import HomePage from './pages/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./components/App";
+import HomePage from "./pages/HomePage";
+import CoursePage from "./pages/CoursePage";
+import CourseListPage from "./pages/CourseListPage";
+import WishlistPage from "./pages/WishlistPage";
 
 function Main() {
   return (
-    <App>
-      <HomePage />
-    </App>
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="courses" element={<CourseListPage />} />
+          <Route
+            path="courses/react-frontend-development"
+            element={<CoursePage />}
+          />
+          <Route path="wishlist" element={<WishlistPage />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
   );
 }
 
