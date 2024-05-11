5d3651c4e3b1713b38c888f8332527e4e94b02dc
Yean <kjan0929@naver.com>
2024-05-11 09:35:10 +0900

    Apply unique key values

--- foodit/src/components/FoodList.js
+++ foodit/src/components/FoodList.js
@@ -25,7 +25,7 @@ function FoodList({ items, onDelete }) {
     <ul>
       {items.map((item) => {
         return (
-          <li>
+          <li key={item.id}>
             <FoodListItem item={item} onDelete={onDelete}></FoodListItem>
           </li>
         );
