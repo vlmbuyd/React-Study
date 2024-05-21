070f352c45ae4fadf4f0ae04aaac643fbe830aed
Yean <kjan0929@naver.com>
2024-05-21 23:01:20 +0900

    Implement a form in one state

--- moviepedia/src/components/ReviewForm.js
+++ moviepedia/src/components/ReviewForm.js
@@ -2,28 +2,37 @@ import { useState } from "react";
 import "./ReviewForm.css";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(typeof e.target);
+    setValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
   };
 
   return (
-    <form className="ReviewForm">
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+    <form className="ReviewForm" onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input
+        name="rating"
+        value={values.rating}
+        type="number"
+        onChange={handleChange}
+      />
+      <textarea name="content" value={values.content} onChange={handleChange} />
+      <button type="submit">확인</button>
     </form>
   );
 }
