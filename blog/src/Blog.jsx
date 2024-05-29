6c6c9f487c1e246cf2ed9f3164e58dd6387d6cb6
Yean <kjan0929@naver.com>
2024-05-29 21:53:38 +0900

    Create a post using useState

--- blog/src/Blog.jsx
+++ blog/src/Blog.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import Post from "./Post";
 
 const Blog = () => {
-  const blog = {
+  const [title, setTitle] = useState({
     title_1: "리액트로 블로그 만들기",
-    content_1: "리액트로 블로그를 만들어봅시다.",
     title_2: "두 번째 포스트",
+  });
+  const [content, setContent] = useState({
+    content_1: "리액트로 블로그를 만들어봅시다.",
     content_2: "리액트는 재밌습니다.",
-  };
+  });
   return (
     <>
-      <Post title={blog.title_1} content={blog.content_1} />
-      <Post title={blog.title_2} content={blog.content_2} />
+      <span>총 Post 개수 : </span>
+      <Post title={title.title_1} content={title.content_1} />
+      <Post title={content.title_2} content={content.content_2} />
     </>
   );
 };
--- blog/src/Post.jsx
+++ blog/src/Post.jsx
@@ -1,8 +1,12 @@
 const Post = ({ title, content }) => {
+  const dt = new Date();
+  const date =
+    dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
   return (
     <>
       <h1>{title}</h1>
       <p>{content}</p>
+      <p>{date}</p>
     </>
   );
 };
