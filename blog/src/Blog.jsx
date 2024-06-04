518718d0bf5d8c1736bd2767bb8753a8f16c3d31
Yean <kjan0929@naver.com>
2024-06-04 17:18:59 +0900

    Implement post delete function

--- blog/src/Blog.jsx
+++ blog/src/Blog.jsx
@@ -2,19 +2,28 @@ import { useState } from "react";
 import Post from "./Post";
 
 const Blog = () => {
-  const [title, setTitle] = useState({
-    title_1: "리액트로 블로그 만들기",
-    title_2: "두 번째 포스트",
-  });
-  const [content, setContent] = useState({
-    content_1: "리액트로 블로그를 만들어봅시다.",
-    content_2: "리액트는 재밌습니다.",
-  });
+  const [data, setData] = useState([
+    ["리액트로 블로그 만들기", "리액트로 블로그를 만들어봅시다."],
+
+    ["두 번째 포스트", "리액트는 재밌습니다."],
+  ]);
+  const [count, setCount] = useState(data.length);
+
+  const addPost = () => {};
+
+  const deletePost = (num) => {
+    const newData = data.filter((_, index) => index !== num);
+    setData(newData);
+    setCount(newData.length);
+  };
+
   return (
     <>
-      <span>총 Post 개수 : </span>
-      <Post title={title.title_1} content={title.content_1} />
-      <Post title={content.title_2} content={content.content_2} />
+      <span>총 Post 개수 : {count}</span> <br />
+      <button type="button" onClick={addPost}>
+        포스트 추가
+      </button>
+      <Post data={data} clickDelete={deletePost} />
     </>
   );
 };
--- blog/src/Post.jsx
+++ blog/src/Post.jsx
@@ -1,12 +1,24 @@
-const Post = ({ title, content }) => {
+const Post = ({ data, clickDelete }) => {
   const dt = new Date();
   const date =
     dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate();
+
+  const handleDelete = (index) => {
+    clickDelete(index);
+  };
   return (
     <>
-      <h1>{title}</h1>
-      <p>{content}</p>
-      <p>{date}</p>
+      {data.map((item, index) => {
+        return (
+          <div key={index}>
+            <h1>{item[0]}</h1>
+            <p>{item[1]}</p>
+            <span>{date}</span> <br />
+            <button>수정</button>
+            <button onClick={() => handleDelete(index)}>삭제</button>
+          </div>
+        );
+      })}
     </>
   );
 };
