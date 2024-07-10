c2e0ee26a645966a83cf46866aaf209dec00ca5b
Yean <kjan0929@naver.com>
2024-07-10 18:11:43 +0900

    delete directory

--- codestudit/src/components/ProfilePhoto.js
+++
@@ -1,7 +0,0 @@
-import styles from './ProfilePhoto.module.css';
-
-function ProfilePhoto({ photo, name }) {
-  return <img className={styles.profilePhoto} src={photo} alt={name} />;
-}
-
-export default ProfilePhoto;
--- codestudit/src/values.js
+++
@@ -1,4 +0,0 @@
-export const FEED_VARIANT = {
-  HOME_FEED: 'HomeFeed',
-  MY_FEED: 'MyFeed',
-};
