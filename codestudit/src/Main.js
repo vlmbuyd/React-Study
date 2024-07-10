0e330ebe765f3c918bf1f061d3d33318ccdbf995
Yean <kjan0929@naver.com>
2024-07-10 16:35:14 +0900

    Add my feed

--- codestudit/src/Main.js
+++ codestudit/src/Main.js
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './components/App';
 import HomePage from './pages/HomePage';
+import MyFeedPage from './pages/MyFeedPage';
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ function Main() {
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<HomePage />} />
+            <Route path="my-feed" element={<MyFeedPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- codestudit/src/pages/HomePage.js
+++ codestudit/src/pages/HomePage.js
@@ -1,11 +1,12 @@
 import Container from '../components/Container';
 import PostList from '../components/PostList';
+import { FEED_VARIANT } from '../values';
 import styles from './HomePage.module.css';
 
 function HomePage() {
   return (
     <Container className={styles.container}>
-      <PostList />
+      <PostList variant={FEED_VARIANT.HOME_FEED} />
     </Container>
   );
 }
